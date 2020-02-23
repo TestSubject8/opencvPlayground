9c43eb4e07fb902b88dbf6ec2b021c11520eba92
Atreya <agkatreya@gmail.com>
2020-02-23 20:53:20 +0530

    added nice logging features

--- videoAvgFrames.py
+++ videoAvgFrames.py
@@ -4,6 +4,11 @@ import sys
 from tkinter import *
 from colour import *
 
+try:
+    log = sys.argv[2]
+except:
+    log = False
+
 src = VideoCapture(sys.argv[1])
 nFrames = int(src.get(CAP_PROP_FRAME_COUNT))
 srcW = int(src.get(CAP_PROP_FRAME_WIDTH))
@@ -15,12 +20,15 @@ print('Resolution: '+str(srcW)+'x'+str(srcH))
 #srcW = 100
 #srcH = 100
 
-nF = math.floor(nFrames/100)*10
-nH = math.floor(srcH/100)*10
-nW = math.floor(srcW/100)*10
+fuzz = 100
+
+nF = math.floor(nFrames/100)*fuzz
+nH = math.floor(srcH/100)*fuzz
+nW = math.floor(srcW/100)*fuzz
 
 main = Tk()
-disp = Canvas(main, width=nF, height=30)
+mainHeight = 50
+disp = Canvas(main, width=nF, height=mainHeight)
 disp.pack()
 
 #result = [0,0,0]
@@ -48,7 +56,7 @@ def bucket(frame, simple=True):
                     for i in range(3):
                         distance += abs(value[i]-frame[h][w][i])
                     # distance 400 has noticable colours - 600 gives just two - 450 is the best
-                    if distance <= 450:
+                    if distance <= 200:
                         insert = False
 #                    else:
 #                        insert = True
@@ -57,7 +65,8 @@ def bucket(frame, simple=True):
                     for i in range(3):
                         pixel[i] = frame[h][w][i]
                     buckets.append(pixel)
-        print(len(buckets))
+            if log:
+                print(len(buckets))
         if simple:
             for pixel in buckets:
                 for i in range(3):
@@ -68,7 +77,8 @@ def bucket(frame, simple=True):
         else:
             return buckets
     except Exception as e:
-        print("Bucket"+str(e))
+        if (log):
+            print("Bucket"+str(e))
 
 def simpleAvg(frame):
     global nF, nW, nH
@@ -76,7 +86,8 @@ def simpleAvg(frame):
     try:
         for h in range(0,nH):
             for w in range(0,nW):
-    #            print("data: "+str(h)+"x"+str(w)+": "+str(frame[h][w]))
+#        if (log):
+#            print("data: "+str(h)+"x"+str(w)+": "+str(frame[h][w]))
                 for i in range(3):
                     result[i] += (frame[h][w][i]) 
         for i in range(3):
@@ -95,8 +106,9 @@ def simpleDraw(src):
             ret, frame = src.read()
             #res = simpleAvg(frame)
             res = bucket(frame)
-    #        print("Got value %1.2f - %1.2f - %1.2f for frame %4d" % (res[0], res[1], res[2], f))
-            disp.create_rectangle(f,0,f,30,outline='',fill=Color(rgb=tuple(res)))
+#            if(log):
+#                print("Got value %1.2f - %1.2f - %1.2f for frame %4d" % (res[0], res[1], res[2], f))
+            disp.create_rectangle(f,0,f,mainHeight,outline='',fill=Color(rgb=tuple(res)))
         except Exception as e:
             print(res)
             print("Frames: "+str(e))
@@ -109,18 +121,22 @@ def bucketDraw(src):
     global nF, nW, nH
     for f in range(nF):
         try:
+            if not f%fuzz :
+                print("Frame %d of %d\r" %(f, nF), end ='')
             src.set(CAP_PROP_POS_FRAMES, f-1)
             ret, frame = src.read()
             #res = simpleAvg(frame)
             buckets = bucket(frame,False)
             nB = len(buckets)
             for i in range(nB):
-                print("Got value " + str(buckets[i]) + " for " + str(f))
+                if(log):
+                    print("Got value " + str(buckets[i]) + " for " + str(f))
                 for j in range(3):
                     buckets[i][j] /= float(255.0)
-                start = (30 / nB) * i
-                end = (30 / nB) * (1 + i)
-                print("Drawing " + str(buckets[i]) + " from " + str(start) + " to " + str(end))
+                start = (mainHeight / nB) * i
+                end = (mainHeight / nB) * (1 + i)
+                if(log):
+                    print("Drawing " + str(buckets[i]) + " from " + str(start) + " to " + str(end))
                 disp.create_rectangle(f,start,f,end,outline='',fill=Color(rgb=tuple(buckets[i])))
         except Exception as e:
             print(buckets)
